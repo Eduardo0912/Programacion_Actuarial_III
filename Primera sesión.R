b0ac1c7b4c0a7470a221a2b42ccec9ec1afe2a9b
Eduardo Sánchez <lalo.89722@gmail.com>
2017-02-17 14:05:05 -0600

    Sesión 16/02/17

---
+++ Primera sesión.R
@@ -0,0 +1,19 @@
+# Clase de 16/02/2017
+x <- 1
+print(x)
+x
+msg <- "Hola"
+msg
+X<- #esta expresión está incompleta
+...
+x<-1:50
+print(x)
+print(x)
+a<-1
+b<-1L
+#class función que devuelve el tipo de la variable
+
+#Creación de vectores
+#Con la función vector
+
+mi_vector<-vector("integer",5)
